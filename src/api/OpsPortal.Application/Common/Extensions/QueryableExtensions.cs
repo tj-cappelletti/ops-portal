e8f7f2c44f466f9809ea15f558dd55aa7c6a62fa
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-09-30 13:10:25 -0400

    Add QueryableExtensions and PaginatedRequest for pagination support

---
+++ src/api/OpsPortal.Application/Common/Extensions/QueryableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpsPortal.Contracts.Responses;
+
+namespace OpsPortal.Application.Common.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<PaginatedResponse<T>> ToPaginatedResponseAsync<T>(
+        this IQueryable<T> source,
+        int pageNumber,
+        int pageSize,
+        CancellationToken cancellationToken = default)
+    {
+        var count = await source.CountAsync(cancellationToken);
+        var items = await source
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return PaginatedResponse<T>.Create(items, pageNumber, pageSize, count);
+    }
+
+    public static async Task<PaginatedResponse<TResult>> ToPaginatedResponseAsync<TSource, TResult>(
+        this IQueryable<TSource> source,
+        int pageNumber,
+        int pageSize,
+        Func<IQueryable<TSource>, IQueryable<TResult>> mapper,
+        CancellationToken cancellationToken = default)
+    {
+        var count = await source.CountAsync(cancellationToken);
+
+        var paginatedQuery = source
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+
+        var mappedQuery = mapper(paginatedQuery);
+        var items = await mappedQuery.ToListAsync(cancellationToken);
+
+        return PaginatedResponse<TResult>.Create(items, pageNumber, pageSize, count);
+    }
+}
---
+++ src/api/OpsPortal.Application/Common/Models/PaginatedRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace OpsPortal.Application.Common.Models;
+
+public abstract record PaginatedRequest
+{
+    private const int DefaultPageSize = 20;
+    private const int MaxPageSize = 100;
+
+    public int PageNumber { get; init; } = 1;
+
+    public int PageSize { get; init; } = DefaultPageSize;
+
+    public int Skip => (ValidPageNumber - 1) * ValidPageSize;
+
+    public string? SortBy { get; init; }
+
+    public bool SortDescending { get; init; } = false;
+
+    public int ValidPageNumber => PageNumber < 1 ? 1 : PageNumber;
+
+    public int ValidPageSize => PageSize switch
+    {
+        <= 0 => DefaultPageSize,
+        > MaxPageSize => MaxPageSize,
+        _ => PageSize
+    };
+}
