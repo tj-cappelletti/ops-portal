3b0566a0ed70ea1a001ba55077572fe1203f60bd
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-09-30 13:12:05 -0400

    Add AssemblyReference class for application assembly referencing

---
+++ src/api/OpsPortal.Application/AssemblyReference.cs
@@ -0,0 +1,7 @@
+﻿namespace OpsPortal.Application;
+
+public static class AssemblyReference
+{
+    // This class is used to reference the Application assembly
+    // for dependency injection and reflection purposes
+}
