f986862cfcc1241179337e98081e4dbd6db5b3a1
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-01 06:33:41 -0400

    Add equality comparer and for solution stacks

---
+++ src/api/OpsPortal.UnitTests/Helpers/GetAllSolutionStacksEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OpsPortal.Application.Features.SolutionStacks.Queries;
+
+namespace OpsPortal.UnitTests.Helpers;
+
+[ExcludeFromCodeCoverage]
+internal class GetAllSolutionStacksEqualityComparer : IEqualityComparer<GetAllSolutionStacks>
+{
+    public bool Equals(GetAllSolutionStacks? x, GetAllSolutionStacks? y)
+    {
+        return x == y;
+    }
+
+    public int GetHashCode(GetAllSolutionStacks obj)
+    {
+        return obj.GetHashCode();
+    }
+}
