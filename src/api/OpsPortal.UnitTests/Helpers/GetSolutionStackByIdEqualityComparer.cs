10a554265c1ef20032607a68eb20f6d917926312
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-03 10:57:06 -0400

    Add equality comparer for GetSolutionStackById

---
+++ src/api/OpsPortal.UnitTests/Helpers/GetSolutionStackByIdEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OpsPortal.Application.Features.SolutionStacks.Queries;
+
+namespace OpsPortal.UnitTests.Helpers;
+
+[ExcludeFromCodeCoverage]
+internal class GetSolutionStackByIdEqualityComparer : IEqualityComparer<GetSolutionStackById>
+{
+    public bool Equals(GetSolutionStackById? x, GetSolutionStackById? y)
+    {
+        return x == y;
+    }
+
+    public int GetHashCode(GetSolutionStackById obj)
+    {
+        return obj.GetHashCode();
+    }
+}
