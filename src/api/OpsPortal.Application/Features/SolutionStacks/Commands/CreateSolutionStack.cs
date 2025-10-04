183b7393e680c4c02c0cdfe02cde09ab2dcdc350
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-04 07:46:15 -0400

    Place holder for CreateSolutionStack command

---
+++ src/api/OpsPortal.Application/Features/SolutionStacks/Commands/CreateSolutionStack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsPortal.Application.Features.SolutionStacks.Commands
+{
+    internal class CreateSolutionStack
+    {
+    }
+}
