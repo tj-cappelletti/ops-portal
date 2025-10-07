b2c506c7d06a699dfd6545dfb1b79cf20c826f7e
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-07 18:55:44 -0400

    Add GetSolutionStackStatusHandler to handle retrieval of solution stack status by ID

---
+++ src/api/OpsPortal.Application/Features/SolutionStacks/Queries/GetSolutionStackStatusHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OpsPortal.Application.Common.Interfaces;
+using OpsPortal.Contracts.SolutionStacks;
+
+namespace OpsPortal.Application.Features.SolutionStacks.Queries;
+
+public class GetSolutionStackStatusHandler : IRequestHandler<GetSolutionStackStatusById, GetSolutionStackStatusResponse?>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetSolutionStackStatusHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetSolutionStackStatusResponse?> Handle(GetSolutionStackStatusById request, CancellationToken cancellationToken)
+    {
+        var solutionStackStatus = await _context.SolutionStackStatuses
+            .Where(s => s.Id == request.Id)
+            .Select(s => new GetSolutionStackStatusResponse(
+                s.Id,
+                s.Name,
+                s.Slug,
+                s.Description))
+            .SingleOrDefaultAsync(cancellationToken);
+
+        return solutionStackStatus;
+    }
+}
