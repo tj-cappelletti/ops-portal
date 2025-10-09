f62452cf3874efcb1294e33d1e00e9df7761abe0
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-09 19:25:05 -0400

    Refactor SolutionStackStatus to inherit from AuditableEntity and update CreateSystemStatus method to use SeedStatusData

--- src/api/OpsPortal.Domain/Entities/SolutionStackStatus.cs
+++ src/api/OpsPortal.Domain/Entities/SolutionStackStatus.cs
@@ -1,14 +1,23 @@
-﻿using OpsPortal.Domain.Common.Utilities;
+﻿using OpsPortal.Domain.Common.Auditing;
+using OpsPortal.Domain.Common.Utilities;
+using OpsPortal.Domain.Constants;
 
 namespace OpsPortal.Domain.Entities;
 
-public class SolutionStackStatus : Entity
+public class SolutionStackStatus : AuditableEntity
 {
+    public string? Color { get; private set; }
+
     public string? Description { get; private set; }
 
-    public bool IsSystem { get; private set; }
+    public int DisplayOrder { get; private set; }
+
+    public bool IsActive { get; private set; }
+
+    public bool IsSystemStatus { get; private set; }
 
     public string Name { get; private set; }
+
 
     public string Slug { get; private set; }
 
@@ -21,23 +30,31 @@ public class SolutionStackStatus : Entity
     private SolutionStackStatus() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-    public SolutionStackStatus(string name, string description)
-    {
-        Id = Guid.NewGuid();
-        Description = description;
-        Name = name;
-        Slug = SlugGenerator.Generate(name);
-    }
-
-    public static SolutionStackStatus CreateSystemStatus(Guid id, string name, string description, string slug)
+    public static SolutionStackStatus CreateSystemStatus(SeedStatusData seedStatusData)
     {
         return new SolutionStackStatus
         {
-            Id = id,
-            Name = name,
-            Description = description,
-            Slug = slug,
-            IsSystem = true
+            Color = seedStatusData.Color,
+            CreatedAt = SystemDefaults.BaseDateTime,
+            CreatedBy = SystemDefaults.SystemUsers.System.Identifier,
+            CreatedById = SystemDefaults.SystemUsers.System.Id,
+            Description = seedStatusData.Description,
+            DisplayOrder = seedStatusData.DisplayOrder,
+            Id = seedStatusData.Id,
+            IsActive = true,
+            IsSystemStatus = true,
+            Name = seedStatusData.Name,
+            Slug = SlugGenerator.Generate(seedStatusData.Name),
+            UpdatedAt = SystemDefaults.BaseDateTime,
+            UpdatedBy = SystemDefaults.SystemUsers.System.Identifier,
+            UpdatedById = SystemDefaults.SystemUsers.System.Id,
         };
     }
 }
+
+public record SeedStatusData(
+    Guid Id,
+    string Name,
+    string Description,
+    string Color,
+    int DisplayOrder);
