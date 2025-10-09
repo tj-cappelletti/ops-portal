42096f453ad9e033cd4b976b71bd10eaa381238d
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-09 19:24:21 -0400

    Add AuthenticationType and UserStatus enums for user authentication and status management

---
+++ src/api/OpsPortal.Domain/Entities/AuthenticationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsPortal.Domain.Entities
+{
+    public enum AuthenticationType
+    {
+        Local,
+        SingleSignOn,
+        ApiKey,     // API key authentication
+        System      // System/service accounts
+    }
+}
---
+++ src/api/OpsPortal.Domain/Entities/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsPortal.Domain.Entities
+{
+    public enum UserStatus
+    {
+        Active,
+        Inactive,
+        Pending,
+        Suspended
+    }
+}
