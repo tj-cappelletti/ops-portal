8e462daa631fa1b1d6bf56942f8c1724aa0bec1b
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-11 16:53:17 -0400

    Updated projects
    Ensured project references are setup
    Added NuGet packages for projects
    Fixed solution reference for WebApi project

---
+++ src/api/OpsPortal.UnitTests/TestAnnotations.cs
@@ -0,0 +1,19 @@
+﻿namespace OpsPortal.UnitTests;
+
+public static class TestAnnotations
+{
+    public static class TraitNames
+    {
+        public const string Category = "Category";
+    }
+
+    public static class Categories
+    {
+        public const string EndToEnd = "EndToEnd";
+        public const string Functional = "Functional";
+        public const string Integration = "Integration";
+        public const string Performance = "Performance";
+        public const string Security = "Security";
+        public const string Unit = "Unit";
+    }
+}
