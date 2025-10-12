250419262355ffe15df8ab104562066715e77811
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-12 09:08:09 -0400

    Created base entities
    Supports auditing, high volume storage, and standard storage

---
+++ src/api/OpsPortal.Domain/Common/HighVolumeEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace OpsPortal.Domain.Common;
+
+public abstract class HighVolumeEntity
+{
+    public long Id { get; protected set; } // Internal PK for performance
+
+    public Guid PublicId { get; protected set; } = Guid.NewGuid(); // External reference
+}
