4d8218d98f76d5de831e3f6938dfa8f308ff61b5
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-12 14:42:46 -0400

    Created common paginated and search queries

---
+++ src/api/OpsPortal.Application/Common/Queries/PaginatedQuery.cs
@@ -0,0 +1,27 @@
+﻿namespace OpsPortal.Application.Common.Queries;
+
+public abstract record PaginatedQuery
+{
+    private const int DefaultPageSize = 20;
+    private const int MaxPageSize = 100;
+
+    public int PageNumber { get; init; } = 1;
+
+    public int PageSize { get; init; } = DefaultPageSize;
+
+    internal int Skip => (GetValidPageNumber() - 1) * GetValidPageSize();
+
+    public string? SortBy { get; init; }
+
+    public bool SortDescending { get; init; } = false;
+
+    public int GetValidPageNumber()
+    {
+        return Math.Max(1, PageNumber);
+    }
+
+    public int GetValidPageSize()
+    {
+        return Math.Min(Math.Max(1, PageSize), MaxPageSize);
+    }
+}
