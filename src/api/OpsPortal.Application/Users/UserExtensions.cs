19de17d668bb32a4d153512d1dd9aca6fa0127fa
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-24 06:47:43 -0400

    Add UserExtensions class with ToUserResponse method for user data transformation

---
+++ src/api/OpsPortal.Application/Users/UserExtensions.cs
@@ -0,0 +1,38 @@
+﻿using OpsPortal.Domain.Entities;
+using OpsPortal.Contracts.Users;
+
+namespace OpsPortal.Application.Users;
+
+public static class UserExtensions
+{
+    public static UserResponse ToUserResponse(this User user)
+    {
+        return new UserResponse(
+            user.AvatarUrl,
+            user.DisplayName,
+            user.Email,
+            user.ExternalId,
+            user.ExternalMetadata,
+            user.FailedLoginAttempts,
+            user.FirstName,
+            user.Id,
+            user.Identifier,
+            user.IdentityProvider,
+            user.IsDeleted,
+            user.IsLocked,
+            user.IsSystemUser,
+            user.LastLoginAt,
+            user.LastLoginIp,
+            user.LastName,
+            user.Locale,
+            user.LockedUntil,
+            user.LoginCount,
+            user.PasswordChangedAt,
+            user.Preferences,
+            user.RefreshTokenExpiry,
+            user.RequirePasswordChange,
+            user.Status.ToString(),
+            user.TimeZone
+        );
+    }
+}
