2706c7d38fdb1d8d4458b929588426bdbf228de9
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-24 06:48:12 -0400

    Add CreateLocalUserRequest record for local user creation

---
+++ src/api/OpsPortal.Contracts/Users/CreateLocalUserRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace OpsPortal.Contracts.Users;
+
+public record CreateLocalUserRequest(
+    string? AvatarUrl,
+    string DisplayName,
+    string? Email,
+    string? FirstName,
+    string Identifier,
+    string? LastName,
+    string? Locale,
+    string Password,
+    bool? RequirePasswordChange,
+    string? TimeZone
+);
