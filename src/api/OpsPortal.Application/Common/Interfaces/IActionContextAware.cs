659461f3a6da4b31dceb63c2746b085b7426c9f1
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-24 06:45:37 -0400

    Add IActionContextAware interface for action context management

---
+++ src/api/OpsPortal.Application/Common/Interfaces/IActionContextAware.cs
@@ -0,0 +1,8 @@
+﻿using OpsPortal.Application.Auditing;
+
+namespace OpsPortal.Application.Common.Interfaces;
+
+public interface IActionContextAware
+{
+    ActionContext? ActionContext { get; set; }
+}
