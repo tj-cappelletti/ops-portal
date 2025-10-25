b3876f636ad038f507155f07befabe3cd74c1376
TJ Cappelletti <tcappelletti@microsoft.com>
2025-10-25 13:35:26 -0400

    Added logging to user creation

--- src/api/OpsPortal.WebApi/Controllers/UsersController.cs
+++ src/api/OpsPortal.WebApi/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using OpsPortal.Application.Users.Commands;
 using OpsPortal.Application.Users.Queries;
 using OpsPortal.Contracts.Common;
@@ -23,26 +22,48 @@ public class UsersController : ApiControllerBase<UsersController>
     [ProducesResponseType(typeof(UserResponse), StatusCodes.Status201Created)]
     public async Task<IActionResult> CreateLocalUser([FromBody]CreateLocalUserRequest request)
     {
+        var loggerState = new Dictionary<string, object>
+        {
+            ["Operation"] = nameof(CreateLocalUser),
+            ["Email"] = request.Email ?? string.Empty,
+            ["RequestId"] = HttpContext.TraceIdentifier,
+            ["CorrelationId"] = CorrelationId ?? HttpContext.TraceIdentifier,
+            ["ClientIp"] = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown"
+        };
 
-        var command = new CreateLocalUserCommand(
-            request.AvatarUrl,
-            request.DisplayName,
-            request.Email,
-            request.FirstName,
-            request.Identifier,
-            request.LastName,
-            request.Locale,
-            request.Password,
-            request.RequirePasswordChange,
-            request.TimeZone);
+        using (Logger.BeginScope(loggerState))
+        {
+            Logger.LogDebug("Creating CreateLocalUserCommand");
+            var command = new CreateLocalUserCommand(
+                request.AvatarUrl,
+                request.DisplayName,
+                request.Email,
+                request.FirstName,
+                request.Identifier,
+                request.LastName,
+                request.Locale,
+                request.Password,
+                request.RequirePasswordChange,
+                request.TimeZone);
 
-        var operationResult = await _mediator.Send(command);
+            Logger.LogDebug(
+                "Created CreateLocalUserCommand with Identifier '{Identifier}' and email '{Email}'",
+                request.Identifier,
+                request.Email);
 
-        // The operationResult.value will be non-null if IsSuccess is true
-        // If it is null, let it throw to be caught by the global exception handler
-        return operationResult.IsSuccess
-            ? CreatedAtAction(nameof(GetUserById), new { id = operationResult.Value!.Id }, operationResult.Value)
-            : HandleOperationError(operationResult.Error);
+            Logger.LogInformation("Sending CreateLocalUserCommand to mediator");
+            var operationResult = await _mediator.Send(command);
+
+            Logger.LogInformation(
+                "CreateLocalUserCommand completed with success: {IsSuccess}",
+                operationResult.IsSuccess);
+
+            // The operationResult.value will be non-null if IsSuccess is true
+            // If it is null, let it throw to be caught by the global exception handler
+            return operationResult.IsSuccess
+                ? CreatedAtAction(nameof(GetUserById), new { id = operationResult.Value!.Id }, operationResult.Value)
+                : HandleOperationError(operationResult.Error);
+        }
     }
 
     [HttpGet]
