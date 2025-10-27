4f5d4a61c51d4904d42df0da15fc9fe9c5e5c2df
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-27 09:15:44 -0400

    Refactored GetUserByIdHandler
    Updated to return OperationResult

--- src/api/OpsPortal.Application/Users/Queries/GetUserById.cs
+++ src/api/OpsPortal.Application/Users/Queries/GetUserById.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
+using OpsPortal.Application.Common;
 using OpsPortal.Contracts.Users;
 
 namespace OpsPortal.Application.Users.Queries;
 
-public record GetUserById(Guid Id) : IRequest<UserResponse?>;
+public record GetUserById(Guid Id) : IRequest<OperationResult<UserResponse>>;
--- src/api/OpsPortal.Application/Users/Queries/GetUserByIdHandler.cs
+++ src/api/OpsPortal.Application/Users/Queries/GetUserByIdHandler.cs
@@ -1,11 +1,12 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using OpsPortal.Application.Common;
 using OpsPortal.Application.Common.Interfaces;
 using OpsPortal.Contracts.Users;
 
 namespace OpsPortal.Application.Users.Queries;
 
-public class GetUserByIdHandler : IRequestHandler<GetUserById, UserResponse?>
+public class GetUserByIdHandler : IRequestHandler<GetUserById, OperationResult<UserResponse>>
 {
     private readonly IApplicationDbContext _context;
 
@@ -14,11 +15,13 @@ public class GetUserByIdHandler : IRequestHandler<GetUserById, UserResponse?>
         _context = context;
     }
 
-    public async Task<UserResponse?> Handle(GetUserById request, CancellationToken cancellationToken)
+    public async Task<OperationResult<UserResponse>> Handle(GetUserById request, CancellationToken cancellationToken)
     {
         var user = await _context.Users
             .SingleOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
 
-        return user?.ToUserResponse();
+        return user == null
+            ? OperationResult<UserResponse>.Failure(OperationError.CreateNotFoundOperationError("User", request.Id))
+            : OperationResult<UserResponse>.Success(user.ToUserResponse());
     }
 }
