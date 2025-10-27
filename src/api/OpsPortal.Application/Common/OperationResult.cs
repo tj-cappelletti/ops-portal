0c3f5054a5e9346c049a55aed1103b28d1b7b9a4
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-10-27 15:19:09 -0400

    Refactor OperationResult methods to private access modifier
    Favor using implicit operators

--- src/api/OpsPortal.Application/Common/OperationResult.cs
+++ src/api/OpsPortal.Application/Common/OperationResult.cs
@@ -17,25 +17,10 @@ public class OperationResult<TValue>
         IsSuccess = isSuccess;
     }
 
-    public static OperationResult<TValue> Failure(OperationError error)
+    private static OperationResult<TValue> Failure(OperationError error)
     {
         return new OperationResult<TValue>(default, error, false);
     }
-
-    //public OperationResult<TNewValue> Map<TNewValue>(Func<TValue, TNewValue> mapper)
-    //{
-    //    return IsSuccess
-    //        ? OperationResult<TNewValue>.Success(mapper(Value!))
-    //        : OperationResult<TNewValue>.Failure(Error!);
-    //}
-
-    //public async Task<OperationResult<TNewValue>> MapAsync<TNewValue>(
-    //    Func<TValue, Task<TNewValue>> mapper)
-    //{
-    //    return IsSuccess
-    //        ? OperationResult<TNewValue>.Success(await mapper(Value!))
-    //        : OperationResult<TNewValue>.Failure(Error!);
-    //}
 
     public static implicit operator OperationResult<TValue>(TValue value)
     {
@@ -47,7 +32,7 @@ public class OperationResult<TValue>
         return Failure(error);
     }
 
-    public static OperationResult<TValue> Success(TValue value)
+    private static OperationResult<TValue> Success(TValue value)
     {
         return new OperationResult<TValue>(value, null, true);
     }
